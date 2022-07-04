08854f4c79e7080ee3dcd477d5e5caae5602a53b
Christos Fotos <c.foutsikas@gmail.com>
2022-07-04 11:53:10 +0300

    Added Region blocks and changed some values.

---
+++ Assets/Robotic Arm/Scripts/SerialCOM.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.IO.Ports;
+
+public class SerialCOM : MonoBehaviour
+{
+    #region
+    //variable decleration field
+    //Port name
+    public string port = "COM3";
+    //Port speed in bps
+    public int baudrate = 9600;
+
+    //Serial Port Decleration
+    private SerialPort sp;
+
+    //boolean for value reading
+    bool isStreaming;
+
+    //Geometry to modify
+    public GameObject sph;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isStreaming)
+        {
+            string value = ReadSerialPort();
+            if (value !=null && float.Parse(value) >= 1.0f)
+            {
+                Debug.Log(value);
+            }
+        }
+    }
+
+    //Open Serial Port and set the program to read values from it.
+    public void Open()
+    {
+        isStreaming = true;
+        sp = new SerialPort(port, baudrate);
+        sp.ReadTimeout = 100;
+        sp.Open(); //Opens the Serial Port
+        Debug.Log("Port connection was established!");
+    }
+
+    public string ReadSerialPort(int timeout = 50)
+    {
+        string message;
+
+        sp.ReadTimeout = timeout;
+        //attempt to read values from serial port
+        try
+        {
+            message = sp.ReadLine();
+            return message;
+        }
+        catch(TimeoutException)
+        {
+            return null;
+        }
+    }
+}
