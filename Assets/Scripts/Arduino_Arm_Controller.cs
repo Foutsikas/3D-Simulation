8608f7425cbc5540a9ecdf7f77712b756a6ba083
Christos Fotos <c.foutsikas@gmail.com>
2022-07-07 16:15:30 +0300

    Started Arduino Control over the Robot

--- Assets/Scripts/Arduino_Arm_Controller.cs
+++ Assets/Scripts/Arduino_Arm_Controller.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Arduino_Arm_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   private SerialCOM sc;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   #region SetUp Variables
+        public int BaseRotationRate = 1;
+        public int baseYRotation = 80;
+        public int BaseNumber;
+
+   #endregion
+
+   #region SetUp Parts
+        // These slots are where you will plug in the appropriate arm parts into the inspector.
+        public Transform robotBase;
+        public Transform upperArm;
+        public Transform lowerArm;
+        public Transform clawPart;
+        public Transform cOpenCloseLeft;
+        public Transform cOpenCloseRight;
+    #endregion
+
+   void _ArmMovement()
+   {
+        baseYRotation = BaseRotationRate * BaseNumber;
+        robotBase.localEulerAngles = new Vector3(robotBase.localEulerAngles.x, baseYRotation, robotBase.localEulerAngles.z);
+   }
 }
