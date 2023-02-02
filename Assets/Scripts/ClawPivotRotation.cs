c518a6e6ffc8d9e69dca720e4e2468e1481b1b90
Christos Fotos <c.foutsikas@gmail.com>
2023-02-02 16:30:15 +0200

    Update ClawPivotRotation.cs
    I might work on this further in the future,
    does not work completely as I envisioned it to.

--- Assets/Scripts/ClawPivotRotation.cs
+++ Assets/Scripts/ClawPivotRotation.cs
@@ -5,7 +5,7 @@ public class ClawPivotRotation : MonoBehaviour
     public Transform UpperJointPivot;
     public Transform ClawPivot;
     private float ZDistance;
-    private Vector3 GroundZ = new Vector3(0, 0, 0);
+    public Transform GroundZ;// = new Vector3(0, 0, 0);
     private Vector3 ClawZ;
 
     private readonly float lerpTime = 1.5f;
@@ -20,16 +20,17 @@ public class ClawPivotRotation : MonoBehaviour
     {
         var x = UpperJointPivot.localEulerAngles.x;
         ClawZ = new Vector3(x, 0, 0);
-        ZDistance = GroundZ.x - ClawZ.x;
-        if (ZDistance == 0)
+        ZDistance = GroundZ.position.x - ClawZ.x;
+        Debug.Log("ZDistance: " + ZDistance);
+        if (Mathf.Abs(ZDistance) < 0.01f)
         {
-            ClawPivot.localRotation = Quaternion.Slerp(ClawPivot.localRotation, Quaternion.AngleAxis(0, Vector3.left), Time.deltaTime * lerpTime);
+            ClawPivot.localRotation = Quaternion.Slerp(ClawPivot.localRotation, Quaternion.AngleAxis(0, Vector3.right), Time.deltaTime * lerpTime);
         }
-        else if (ZDistance < 0)
+        else if (ZDistance < 0f)
         {
-            ClawPivot.localRotation = Quaternion.Slerp(ClawPivot.localRotation, Quaternion.AngleAxis(50, Vector3.left), Time.deltaTime * lerpTime);
+            ClawPivot.localRotation = Quaternion.Slerp(ClawPivot.localRotation, Quaternion.AngleAxis(-50, -Vector3.right), Time.deltaTime * lerpTime);
         }
-        else if (ZDistance > 0)
+        else if (ZDistance > 0f)
         {
             ClawPivot.localRotation = Quaternion.Slerp(ClawPivot.localRotation, Quaternion.AngleAxis(50, Vector3.right), Time.deltaTime * lerpTime);
         }
