2ef9eeefe941807084f10b8da693ba48a0617f2f
Christos Fotos <c.foutsikas@gmail.com>
2023-02-10 16:28:19 +0200

    Update SerialCOMSliders.cs

--- Assets/Scripts/SerialCOMSliders.cs
+++ Assets/Scripts/SerialCOMSliders.cs
@@ -9,6 +9,7 @@ public class SerialCOMSliders : MonoBehaviour
 {
     public static SerialCOMSliders i;
     public static ControlledBySlider cbs;
+
     #region Serial Port Communication Initializer
     //variable decleration field
 
@@ -43,5 +44,61 @@ public class SerialCOMSliders : MonoBehaviour
         private float ClawRotRight = cbs.ClawRotRight;
     #endregion
 
-    
+    private void Start()
+    {
+        //Open serial port and start a read thread
+        sp = new SerialPort("COM4", baudrate);
+        sp.Open();
+        sp.ReadTimeout = 1;
+        isStreaming = true;
+        readThread = new Thread(StreamIn);
+        readThread.Start();
+    }
+
+    private void StreamIn()
+    {
+        while (isStreaming)
+        {
+            try
+            {
+                incomingValue = sp.ReadLine();
+                // Debug.Log("Serial input: " + incomingValue);
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogWarning(e.Message);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        baseValue = cbs.baseRotation;
+        upperArmRotation = cbs.upperArmRotation;
+        lowerArmRotation = cbs.lowerArmRotation;
+        ClawRotLeft = cbs.ClawRotLeft;
+        ClawRotRight = cbs.ClawRotRight;
+
+    if (sp?.IsOpen == true)
+    {
+        //Send the slider values as 4 different strings
+        string baseValueString = baseValue.ToString();
+        string upperArmRotationString = upperArmRotation.ToString();
+        string lowerArmRotationString = lowerArmRotation.ToString();
+        string ClawRotString = (ClawRotLeft + ClawRotRight).ToString();
+
+        //Write the values to the serial port
+        sp.WriteLine(baseValueString);
+        sp.WriteLine(upperArmRotationString);
+        sp.WriteLine(lowerArmRotationString);
+        sp.WriteLine(ClawRotString);
+    }
+    }
+
+    private void OnDestroy()
+    {
+        isStreaming = false;
+        readThread.Join();
+        sp.Close();
+    }
 }
