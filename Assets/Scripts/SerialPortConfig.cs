53712d8147cac95d3b2dba05c5b4e22de5264470
Christos Fotos <c.foutsikas@gmail.com>
2023-02-13 16:13:01 +0200

    SeriaPortConfig
    A small class constructor for setting PortName and BaudRate

---
+++ Assets/Scripts/SerialPortConfig.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerialPortConfig : MonoBehaviour
+{
+    public string PortName { get; set; }
+    public int BaudRate { get; set; }
+}
