ba8cf6b6ecdd581e4d6c72d15a6d621e8ad324cf
Christos Fotos <c.foutsikas@gmail.com>
2023-02-14 15:22:53 +0200

    Update SerialPortConfig.cs
    Removed the MonoBehaviour tag from the file.

--- Assets/Scripts/SerialPortConfig.cs
+++ Assets/Scripts/SerialPortConfig.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SerialPortConfig : MonoBehaviour
+public class SerialPortConfig
 {
     public string PortName { get; set; }
     public int BaudRate { get; set; }
